31e787cfd51108017a1159f9898f845dc70a2d14
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-10-07 16:21:06 -0400

    BookType Added to query books. Lookup by id is now flexible

--- server/app.js
+++ server/app.js
@@ -3,9 +3,12 @@ const PORT = process.env.PORT || 3000;
 const graphqlHTTP = require('express-graphql');
 
 const app = express();
-const graphqlSchema = {}; // Schema def?
+const schema = require('./schema/schema');
 
-app.use('/graphql', graphqlHTTP(graphqlSchema));
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,54 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { 
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID
+} = graphql;
+const _  = require('lodash');
 
-const BookSchema = {
-  name: GraphQLString,
+
+let dummyData = [
+  {name: 'HP1', genre: 'Fantasy', id: '1'},
+  {name: 'Eragon', genre: 'Fantasy', id: '2'},
+  {name: 'Artemis Fowl', genre: 'Fantasy', id: '3'}
+];
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
   fields: () => ({
-    id: GraphQLString,
-    name: GraphQLString,
-    genre: GraphQLString
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    genre: {type: GraphQLString}
   })
-};
+});
 
-const BookType = new GraphQLObjectType(BookSchema);
+// {
+//   book (id: "123"){ // args of type String
+//     name
+//     genre
+//   }
+// }
+
+// RootQuery defines how we can jumo into the graph to query it
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { 
+          id: {
+              type: GraphQLID
+            }
+        },
+      resolve(parent, args) {
+        // code to get data from db
+        return _.find(dummyData, {id: args.id});
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
